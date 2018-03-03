de88af48556ebb636c8f3705231ca75d39490fef
Matheus Paixao <36537253+ninlin089@users.noreply.github.com>
2018-03-03 11:05:12 -0300

    Add files via upload

---
+++ P.A.py
@@ -0,0 +1,24 @@
+print('P.A (Progressão Aritmetica)')
+print('==' * 10)
+primeiro = float(input('1º termo: '))
+razao = float(input('Razão da PA: '))
+termo_visu = int(input("Quantos termos voce deseja ver?"))
+termo = primeiro
+cont = 1
+while cont <= termo_visu:
+    print('{} → '.format(termo), end='')
+    termo += razao
+    cont += 1
+print("")
+
+print('P.G (Progressão Geometrica)')
+print('==' * 10)
+primeiro_pg = float(input('1ºtermo: '))
+razao_pg = float(input('Razão da PG: '))
+termo_visu_pg = int(input("Quantos termos voce deseja ver?"))
+termo_pg = primeiro_pg
+cont_pg = 1
+while cont_pg <= termo_visu_pg:
+    print('{} → '.format(termo_pg), end='')
+    termo_pg *= razao_pg
+cont_pg += 1
